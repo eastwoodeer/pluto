c55d8e238b4dae0a1b5d52fc39897336f192da60
chenbin <chen.bin11@zte.com.cn>
2016-08-18 17:27:17 +0800

    use select

--- proxy.py
+++ proxy.py
@@ -1,4 +1,5 @@
 import socket
+import select
 import urllib
 
 from urllib.parse import urlparse
@@ -6,13 +7,15 @@ from urllib.parse import urlparse
 
 HOST = ''                 # Symbolic name meaning all available interfaces
 PORT = 8765               # Arbitrary non-privileged port
+server_address = (HOST, PORT)
 
 
 class HTTPRequest:
     charset = 'utf-8'
     configs = {}
 
-    def __init__(self, request):
+    def __init__(self, connection, request):
+        self.connection = connection
         self.request = request.decode('utf-8')
         contents = self.request.split('\r\n')
         self.method, url, self.proto = contents[0].split()
@@ -32,21 +35,40 @@ Accept-Encoding: {}
 
 class Proxy:
     def server(self):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind((HOST, PORT))
-        s.listen(10)
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.setblocking(False)
+        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server.bind(server_address)
+        server.listen(10)
+        inputs = [server]
 
         while True:
-            conn, addr = s.accept()
-            print('Connected by', addr)
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                request = HTTPRequest(data)
-                data = self.send(request)
-                conn.sendall(data)
-            conn.close()
+            readable, writable, exception = select.select(inputs, [], inputs, 20)
+            if not (readable or writable or exception):
+                print("Timeout")
+                continue
+            for s in readable:
+                if s is server:
+                    conn, addr = server.accept()
+                    print('Connected by', addr)
+                    conn.setblocking(0)
+                    inputs.append(conn)
+                else:
+                    data = conn.recv(4096)
+                    if not data:
+                        conn.close()
+                        inputs.remove(conn)
+                        break
+                    request = HTTPRequest(conn, data)
+                    # print(data.decode('utf-8'))
+                    # request = HTTPRequest(data)
+                    # data = self.send(request)
+                    # conn.sendall(data)
+                    # conn.close()
+            for s in exception:
+                print('exception on {}'.format(s.getpeername))
+                inputs.remove(s)
+                s.close()
 
     def send(self, request):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
