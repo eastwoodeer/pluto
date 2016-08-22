b9d1d4e0b49e30d3b47f22f0602802e32aa3d63c
chenbin <chen.bin11@zte.com.cn>
2016-08-22 14:42:47 +0800

    实现GET方法

--- proxy.py
+++ proxy.py
@@ -36,31 +36,51 @@ Accept-Encoding: {}
 class Proxy:
     def server(self):
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server.setblocking(False)
+        server.setblocking(0)
         server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         server.bind(server_address)
         server.listen(10)
         inputs = [server]
+        requests = {}
 
         while True:
             readable, writable, exception = select.select(inputs, [], inputs, 20)
             if not (readable or writable or exception):
+                print(readable)
+                print(exception)
                 print("Timeout")
                 continue
             for s in readable:
                 if s is server:
-                    conn, addr = server.accept()
+                    client, addr = s.accept()
                     print('Connected by', addr)
-                    conn.setblocking(0)
-                    inputs.append(conn)
-                else:
-                    data = conn.recv(4096)
+                    client.setblocking(0)
+                    inputs.append(client)
+                elif s in requests:
+                    data = s.recv(4096)
                     if not data:
-                        conn.close()
-                        inputs.remove(conn)
-                        break
-                    request = HTTPRequest(conn, data)
-                    # print(data.decode('utf-8'))
+                        s.close()
+                        inputs.remove(s)
+                        req = requests[s]
+                        req.connection.close()
+                        inputs.remove(req.connection)
+                        del requests[s]
+                        continue
+                    req = requests.get(s, None)
+                    if req:
+                        req.connection.send(data)
+                else:
+                    data = s.recv(4096)
+                    if not data:
+                        print('>>>>>>>>>>no data<<<<<<<<<<<<', s)
+                        s.close()
+                        inputs.remove(s)
+                        continue
+                    request = HTTPRequest(s, data)
+                    print(data.decode('utf-8'))
+                    request_sock = self.send(request)
+                    inputs.append(request_sock)
+                    requests[request_sock] = request
                     # request = HTTPRequest(data)
                     # data = self.send(request)
                     # conn.sendall(data)
@@ -79,13 +99,15 @@ class Proxy:
         # s.connect((url.netloc, port))
         s.connect(('10.74.120.140', 8000))
         msg = 'GET {} HTTP/1.1\r\nHost: {}\r\n\r\n'.format(url.path, url.netloc)
+        s.setblocking(0)
         s.send(msg.encode('utf-8'))
-        data = s.recv(4096)
-        recv_data = data
-        while len(data):
-            data = s.recv(4096)
-            recv_data += data
-        return recv_data
+        return s
+        # data = s.recv(4096)
+        # recv_data = data
+        # while len(data):
+        #     data = s.recv(4096)
+        #     recv_data += data
+        # return recv_data
 
 
 if __name__ == '__main__':
