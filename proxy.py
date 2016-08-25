a8e9f8204c5892d31e7e7868fc17fab135df12e1
chenbin <heychenbin@163.com>
2016-08-25 23:48:36 +0800

    不考虑异常情况，基本实现http代理了 测试可用食用app.yinxiang.com等https网站

--- proxy.py
+++ proxy.py
@@ -29,38 +29,56 @@ class HTTPRequest:
 
 
 class Proxy:
+    def __init__(self, loop):
+        self.loop = loop
+
     async def server(self, reader, writer):
         data = await reader.read(4096)
         message = data.decode()
-        print(message)
         request = HTTPRequest(data)
+        print(request)
         if request.method == 'CONNECT':
             await self.connect_data(request, reader, writer)
         else:
             await self.get_data(request, writer)
 
     async def connect_data(self, request, client_reader, client_writer):
-        reader, writer = await asyncio.open_connection(request.url, 443, ssl=True)
+        reader, writer = await asyncio.open_connection(request.url, 443)
         msg = 'HTTP/1.1 200 Connection Established\r\n\r\n'
-        print('===========================')
-        client_writer.write(msg.encode('latin-1'))
-        while True:
-            data = await client_reader.read()
+        client_writer.write(msg.encode())
+        client = client_writer.get_extra_info('socket')
+        server = writer.get_extra_info('socket')
+        self.loop.add_reader(client, self.client_read, server, client)
+        self.loop.add_reader(server, self.server_read, server, client)
+
+    def client_read(self, server, client):
+        try:
+            data = client.recv(4096)
             if not data:
-                client_writer.close()
-                writer.close()
-                break
-            writer.write(data)
-            while True:
-                data = await reader.read(4096)
-                if not data:
-                    break
-                client_writer.write(data)
+                print('remove client_read')
+                self.loop.remove_reader(client)
+                return
+            server.send(data)
+        except Exception as e:
+            print('client read exception: {}'.format(e))
+            self.loop.remove_reader(client)
+
+    def server_read(self, server, client):
+        try:
+            data = server.recv(4096)
+            if not data:
+                print('remove server_read')
+                self.loop.remove_reader(server)
+                return
+            client.send(data)
+        except Exception as e:
+            print('server read exception: {}'.format(e))
+            self.loop.remove_reader(server)
+
 
     async def get_data(self, request, client):
         url = request.url
-        reader, writer = await asyncio.open_connection('10.74.120.140', 80)
-        # reader, writer = await asyncio.open_connection(url.netloc, 80)
+        reader, writer = await asyncio.open_connection(url.netloc, 80)
         msg = '{} {} HTTP/1.1\r\n{}'.format(request.method, url.path, request.content)
         writer.write(msg.encode('latin-1'))
         while True:
@@ -71,21 +89,28 @@ class Proxy:
                 break
             client.write(data)
 
+    def read(self, server, client):
+        data = server.recv(4096)
+        if not data:
+            self.loop.remove_reader(server)
+            return
+        client.write(data)
+
+
+    def run(self):
+        coro = asyncio.start_server(self.server, HOST, PORT, loop=self.loop)
+        server = self.loop.run_until_complete(coro)
+        print('Serving on {}'.format(server.sockets[0].getsockname()))
+        try:
+            self.loop.run_forever()
+        except KeyboardInterrupt:
+            pass
+        server.close()
+        self.loop.run_until_complete(server.wait_closed())
+        self.loop.close()
+
 
 if __name__ == '__main__':
-    proxy = Proxy()
     loop = asyncio.get_event_loop()
-    coro = asyncio.start_server(proxy.server, HOST, PORT, loop=loop)
-    server = loop.run_until_complete(coro)
-
-    # Serve requests until Ctrl+C is pressed
-    print('Serving on {}'.format(server.sockets[0].getsockname()))
-    try:
-        loop.run_forever()
-    except KeyboardInterrupt:
-        pass
-
-    # Close the server
-    server.close()
-    loop.run_until_complete(server.wait_closed())
-    loop.close()
+    proxy = Proxy(loop)
+    proxy.run()
