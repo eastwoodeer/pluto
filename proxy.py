d2fb55dba01b4de05e3a51b5c6613a46e973305b
chenbin <heychenbin@163.com>
2016-08-29 23:02:04 +0800

    监听写操作，避免写错误。
    基本上可用了，测试https，http以及视频等都OK了，只是现在http头解析还有点
    简单粗暴，后续再优化了。

--- proxy.py
+++ proxy.py
@@ -1,14 +1,14 @@
+import argparse
 import asyncio
+import logging
 import socket
-import select
-import urllib
 
 from urllib.parse import urlparse
 
 
-HOST = ''                 # Symbolic name meaning all available interfaces
-PORT = 8765               # Arbitrary non-privileged port
-CRLF = '\r\n'
+log = logging.getLogger(__name__)
+
+CRLF, COLON, SPACE = '\r\n', ':', ' '
 
 class HTTPRequest(object):
     charset = 'utf-8'
@@ -21,10 +21,10 @@ class HTTPRequest(object):
         self.method, url, self.proto = contents[0].split()
         self.url = urlparse(url)
         if self.method == 'CONNECT':
-            self.hostname, self.port = url.split(':')
+            self.hostname, self.port = url.split(COLON)
         else:
-            if ':' in self.url.netloc:
-                self.hostname, self.port = self.url.netloc.split(':')
+            if COLON in self.url.netloc:
+                self.hostname, self.port = self.url.netloc.split(COLON)
             else:
                 self.hostname = self.url.netloc
         self.content = CRLF.join(contents[1:])
@@ -35,10 +35,20 @@ class HTTPRequest(object):
 
 class Connection(object):
     def __init__(self, reader, writer):
+        self.buffer = b''
         self.closed = False
         self.reader = reader
         self.writer = writer
         self.socket = writer.get_extra_info('socket')
+
+    def buffer_size(self):
+        return len(self.buffer)
+
+    def has_buffer(self):
+        return self.buffer_size() > 0
+
+    def queue(self, data):
+        self.buffer += data
 
     def recv(self, bytes=8192):
         return self.socket.recv(bytes)
@@ -52,6 +62,10 @@ class Connection(object):
     def write(self, data):
         self.writer.write(data)
 
+    def flush(self):
+        sent = self.send(self.buffer)
+        self.buffer = self.buffer[sent:]
+
     def close(self):
         self.writer.close()
         self.closed = True
@@ -59,12 +73,12 @@ class Connection(object):
 
 class Client(Connection):
     def __str__(self):
-        return 'Client closed: {}'.format(self.closed)
+        return 'Client'
 
 
 class Server(Connection):
     def __str__(self):
-        return 'Server closed: {}'.format(self.closed)
+        return 'Server'
 
 
 class ProxyError(Exception):
@@ -72,7 +86,6 @@ class ProxyError(Exception):
 
 
 class ProxyConnectionFailed(ProxyError):
-
     def __init__(self, host, port, reason):
         self.host = host
         self.port = port
@@ -86,6 +99,10 @@ class Proxy(object):
     def __init__(self, loop):
         self.loop = loop
         # self.server = None
+        self.https_connection_established = CRLF.join([
+            'HTTP/1.1 200 Connection Established',
+            'Proxy-agent: proxy.py',
+            CRLF]).encode()
 
     async def connect(self, request):
         try:
@@ -101,18 +118,21 @@ class Proxy(object):
             self.client.close()
             return
         request = HTTPRequest(data)
-        print(request)
+        log.debug(request)
+        log.info('{} {} {}'.format(request.method,
+                                   request.hostname,
+                                   request.url.path))
         await self.process_request(request)
 
     async def process_request(self, request):
         try:
             self.server = await self.connect(request)
         except ProxyConnectionFailed as e:
-            print(e)
+            log.exception(e)
             self.client.close()
             return
         if request.method == 'CONNECT':
-            self.client.write('HTTP/1.1 200 Connection Established\r\n\r\n'.encode())
+            self.client.queue(self.https_connection_established)
         else:
             if request.url.query:
                 path = '?'.join([request.url.path, request.url.query])
@@ -121,47 +141,50 @@ class Proxy(object):
             msg = '{} {} HTTP/1.1\r\n{}'.format(request.method,
                                                 path,
                                                 request.content)
-            self.server.write(msg.encode())
-        self.loop.add_reader(self.client.socket, self.client_read)
-        self.loop.add_reader(self.server.socket, self.server_read)
+            self.server.queue(msg.encode())
+        self.loop.add_reader(self.client.socket, self.read, self.client, self.server)
+        self.loop.add_reader(self.server.socket, self.read, self.server, self.client)
+        self.loop.add_writer(self.client.socket, self.write, self.client)
+        self.loop.add_writer(self.server.socket, self.write, self.server)
 
-    def client_read(self):
+    def write(self, writer):
         try:
-            data = self.client.recv()
-            if not data:
-                self.loop.remove_reader(self.client.socket)
-                return
-            self.server.write(data)
+            writer.flush()
         except Exception as e:
-            print('client read exception: {}'.format(e))
-            self.loop.remove_reader(self.client.socket)
-            self.client.close()
+            log.debug('{} write exception: {}'.format(writer, e))
+            self.loop.remove_writer(writer.socket)
+            # TODO: need to close this?
+            writer.close()
 
-    def server_read(self):
+    def read(self, reader, writer):
         try:
-            data = self.server.recv()
+            data = reader.recv()
             if not data:
-                self.loop.remove_reader(self.server.socket)
+                self.loop.remove_reader(reader.socket)
+                reader.close()
                 return
-            self.client.write(data)
+            writer.queue(data)
         except Exception as e:
-            print('server read exception: {}'.format(e))
-            self.loop.remove_reader(self.server.socket)
-            self.server.close()
+            log.debug('{} read exception: {}'.format(reader, e))
+            self.loop.remove_reader(reader.socket)
+            reader.close()
 
 
 class Pluto(object):
-    def __init__(self, loop):
+    def __init__(self, loop, hostname, port):
         self.loop = loop
+        self.hostname = hostname
+        self.port = port
 
     async def start(self, reader, writer):
         proxy = Proxy(self.loop)
         await proxy.server(reader, writer)
 
     def run(self):
-        coro = asyncio.start_server(self.start, HOST, PORT, loop=self.loop)
+        coro = asyncio.start_server(self.start, self.hostname,
+                                    self.port, loop=self.loop)
         server = self.loop.run_until_complete(coro)
-        print('Serving on {}'.format(server.sockets[0].getsockname()))
+        log.info('Serving on {}'.format(server.sockets[0].getsockname()))
         try:
             self.loop.run_forever()
         except KeyboardInterrupt:
@@ -171,7 +194,23 @@ class Pluto(object):
         self.loop.close()
 
 
-if __name__ == '__main__':
+def main():
+    parser = argparse.ArgumentParser(description='proxy.py')
+    parser.add_argument('--hostname', default='127.0.0.1', help='Default: 127.0.0.1')
+    parser.add_argument('--port', default='8765', help='Default: 8765')
+    parser.add_argument('--log-level', default='INFO',
+                        help='DEBUG, INFO, WARNING, ERROR, CRITICAL')
+    args = parser.parse_args()
+
+    logging.basicConfig(level=getattr(logging, args.log_level),
+                        format='%(asctime)s - %(levelname)s - pid:%(process)d - %(message)s')
+    hostname = args.hostname
+    port = int(args.port)
+
     loop = asyncio.get_event_loop()
-    server = Pluto(loop)
+    server = Pluto(loop, hostname, port)
     server.run()
+
+
+if __name__ == '__main__':
+    main()
